197af02945d998ed5ca1c0916fa50540d1f2c691
afew3232 <60533831+afew3232@users.noreply.github.com>
2020-04-10 16:45:49 +0900

    [Add]migrate

--- db/migrate/20200410054911_create_tags.rb
+++ db/migrate/20200410054911_create_tags.rb
@@ -2,7 +2,7 @@ class CreateTags < ActiveRecord::Migration[5.2]
   def change
     create_table :tags do |t|
       t.integer :post_id
-      t.string :name
+      t.string :name, null: false
 
       t.timestamps
     end
