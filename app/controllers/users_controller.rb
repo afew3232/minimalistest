60b82e655ab4635d4e1fad2da667869363b0812f
afew3232 <60533831+afew3232@users.noreply.github.com>
2020-04-14 19:46:46 +0900

    [Add] 会員画面マイページ& edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,24 @@
 class UsersController < ApplicationController
   def show
+  	@user = User.find(params[:id])
   end
 
   def edit
+  	@user = User.find(params[:id])
   end
+
+  def update
+  	@user = User.find(params[:id])
+  	if @user.update(params_user)
+  		redirect_to user_path(@user.id)
+  	else
+  		render :edit
+  	end
+  end
+
+  private
+  def params_user
+  	params.require(:user).permit(:email, :password, :nickname, :lastname, :lastname_kana, :firstname, :firstname_kana)
+  end
+
 end
