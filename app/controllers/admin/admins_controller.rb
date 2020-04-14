51c388c43caffa06edbd167d4ed4444664c9c8fe
afew3232 <60533831+afew3232@users.noreply.github.com>
2020-04-14 14:12:00 +0900

    [Add] admin show edit

--- app/controllers/admin/admins_controller.rb
+++ app/controllers/admin/admins_controller.rb
@@ -1,7 +1,24 @@
 class Admin::AdminsController < ApplicationController
   def show
+  	@admin = Admin.find(params[:id])
   end
 
   def edit
+  	@admin = Admin.find(params[:id])
   end
+
+  def update
+  	@admin = Admin.find(params[:id])
+  	if @admin.update(params_admin)
+  		redirect_to admin_admin_path(@admin.id)
+  	else
+  		render :edit
+  	end
+  end
+
+  private
+  def params_admin
+  	params.require(:admin).permit(:email)
+  end
+
 end
