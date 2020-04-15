0171c26bc9a2d5fed1d345d5138337e1c3b51015
afew3232 <60533831+afew3232@users.noreply.github.com>
2020-04-15 17:04:31 +0900

    [Fix] 会員画面 post edit レイアウト修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_14_075233) do
+ActiveRecord::Schema.define(version: 2020_04_15_075920) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
