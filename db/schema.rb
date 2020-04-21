b01392e9fb0e6cc0cafb0b5c3e5fb37e7bdd183a
afew3232 <60533831+afew3232@users.noreply.github.com>
2020-04-21 15:54:04 +0900

    [Fix] 管理、会員画面 記事投稿機能修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_15_084638) do
+ActiveRecord::Schema.define(version: 2020_04_21_043403) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2020_04_15_084638) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_id"
+    t.string "post_image"
   end
 
   create_table "tags", force: :cascade do |t|
