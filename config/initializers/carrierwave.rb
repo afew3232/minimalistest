c432b1313b9a8f46f988e8f12601c487cde77379
afew3232 <60533831+afew3232@users.noreply.github.com>
2020-04-27 18:22:19 +0900

    f

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,16 +5,14 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
   if Rails.env.production?
     config.storage :fog
-    config.asset_host = 'https://minimalistest.s3.amazonaws.com'
-    config.fog_provider = 'fog/aws'
-    config.fog_directory  = 'minimalistest'
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
       region: ENV['AWS_REGION'],
-      path_style: true
     }
+    config.fog_directory  = 'minimalistest'
+    config.fog_provider = 'fog/aws'
   else
     config.storage :file
     config.enable_processing = false if Rails.env.test?
