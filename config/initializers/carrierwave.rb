3ebc81a0cb79f020300a552a51e7d3ac7b213d5f
afew3232 <60533831+afew3232@users.noreply.github.com>
2020-04-27 20:03:45 +0900

    f

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,24 @@
-  CarrierWave.configure do |config|
-    config.storage    =  :aws                  # required
-    config.aws_bucket =  ENV['minimalistest']      # required
-    config.aws_acl    =  :public_read
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
 
-    config.aws_credentials = {
-      access_key_id:      ENV['AWS_ACCESS_KEY_ID'],       # required
-      secret_access_key:  ENV['AWS_SECRET_ACCESS_KEY']     # required
+CarrierWave.configure do |config|
+  if Rails.env.production?
+    config.storage :fog
+    config.fog_directory  = 'minimalistest'
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: ENV['AWS_REGION'],
+      path_style: true
     }
-
-    config.aws_attributes = {
-      'Cache-Control'=>"max-age=#{365.day.to_i}",
-      'Expires'=>'Tue, 29 Dec 2015 23:23:23 GMT'
-    }
+  else
+    config.storage :file
+    config.enable_processing = false if Rails.env.test?
   end
+end
+
+
+CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
