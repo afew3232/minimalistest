1aacbf07da9ee0dcf5e1aea9c5f763c1566c1fa3
afew3232 <60533831+afew3232@users.noreply.github.com>
2020-04-27 15:03:28 +0900

    Revert "[Fix] carrierwaveの画像をS3に保存できるよう修正"

--- app/uploaders/post_image_uploader.rb
+++ app/uploaders/post_image_uploader.rb
@@ -4,27 +4,13 @@ class PostImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.development?
-    storage :file
-  elsif Rails.env.test?
-    storage :file
-  else
-    storage :fog
-  end
+  storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
-
-  def extension_whitelist
-    %w(png jpg)
-  end
-
-  def filename
-    original_filename if original_filename
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
